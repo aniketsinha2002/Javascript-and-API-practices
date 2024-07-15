b9c66dbfaf392454c844d609c7d525a19c998bc6
Aniket <sinhaaniket192@gmail.com>
2024-07-15 23:11:15 +0530

    Search Item Found JS Practice

--- jspractice/script.js
+++ jspractice/script.js
@@ -1,63 +1,28 @@
-document.querySelector("button").addEventListener("click", addTask);
+//given a list of data show if the data is present else show no record found
+const data = [
+  {
+    name: "Aniket",
+    email: "aniket@gmail.com",
+  },
+  {
+    name: "Raj",
+    email: "raj@gmail.com",
+  },
+];
 
-function addTask() {
-  //getting input from input box
-  const taskInput = document.querySelector("input");
+document.querySelector("button").addEventListener("click", showData);
 
-  if (taskInput.value.length != 0) {
-    //appending li to ul
-    const li = document.createElement("li");
-    li.textContent = taskInput.value;
+function showData() {
+  const selectInput = document.querySelector("input");
+  const inputval = selectInput.value;
 
-    //appending li to ul and del btn to li
-    const todo = document.querySelector("ul").appendChild(li);
+  const selectDiv = document.querySelector("div");
 
-    //appending del btn to li
-    const delBtn = document.createElement("button");
-    delBtn.textContent = "Delete";
-    todo.appendChild(delBtn);
+  const result = data.find((item) => item.name === inputval);
 
-    //appending update btn on li
-    const updatebtn = document.createElement("button");
-    updatebtn.textContent = "Update";
-    todo.appendChild(updatebtn);
-
-    //deleting the todo on click
-    delBtn.addEventListener("click", deleteTodo);
-
-    //updating the todo on click
-    updatebtn.addEventListener("click", updateTodo);
-
-    taskInput.value = "";
+  if (result) {
+    selectDiv.innerText = result.email;
+  } else {
+    selectDiv.innerText = "No records Found";
   }
 }
-
-const deleteTodo = (e) => {
-  const li = e.target.parentElement;
-  li.remove();
-};
-
-const updateTodo = (e) => {
-  const btn = e.target;
-  const li = e.target.parentElement;
-
-  if (btn.textContent === "Update") {
-    btn.textContent = "Save";
-
-    // Create an input field with the current task text
-    var tempInput = document.createElement("input");
-    tempInput.type = "text";
-    tempInput.value = li.childNodes[0].textContent;
-
-    //replace that li with input field
-    li.replaceChild(tempInput, li.childNodes[0]);
-  } else {
-    btn.textContent = "Update";
-
-    //create a text node with the current updated text to update li
-    const textNode = document.createTextNode(li.childNodes[0].value);
-
-    //replace that input field with updatedLi
-    li.replaceChild(textNode, li.childNodes[0]);
-  }
-};
